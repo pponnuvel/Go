fc79ebaa5003ef0d37e668b7d448cc039dcee6d3
Ponnuvel Palaniyappan <pponnuvel@gmail.com>
2017-03-09 22:55:23 +0000

    Bubble sort examples

---
+++ bs_int.go
@@ -0,0 +1,33 @@
+// Bubble sort for integers
+package main
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+)
+
+func main() {
+    var tmp string
+    length := len(os.Args)
+
+    for i :=1; i < length; i++ {
+        for j := 1; j < i; j++ {
+            val, err1 := strconv.Atoi(os.Args[i])
+            val2, err2 := strconv.Atoi(os.Args[j])
+            if err1 != nil || err2 != nil {
+                fmt.Println("Invalid input found")
+                os.Exit(1)
+            }
+            if val < val2 {
+                tmp = os.Args[i]
+                os. Args[i] = os.Args[j]
+                os.Args[j] = tmp
+            }
+        }
+    }
+
+    for i := 1; i < length; i++ {
+        fmt.Println(os.Args[i])
+    }
+}
---
+++ bs_string.go
@@ -0,0 +1,26 @@
+// Bubble sort for strings
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    var tmp string
+    length := len(os.Args)
+
+    for i :=1; i < length; i++ {
+        for j := 1; j < i; j++ {
+            if os.Args[i] < os.Args[j] {
+                tmp = os.Args[i]
+                os. Args[i] = os.Args[j]
+                os.Args[j] = tmp
+            }
+        }
+    }
+
+    for i := 1; i < length; i++ {
+        fmt.Println(os.Args[i])
+    }
+}
