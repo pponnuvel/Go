79a78455f8adc0c95a6c55c33396b4d82dd71f43
Ponnuvel Palaniyappan <pponnuvel@gmail.com>
2017-03-09 22:36:37 +0000

    String concatenation

---
+++ strcat.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var res string
+	for i := 1; i < len(os.Args); i++ {
+		if len(res) != 0 {
+			res += "-" + os.Args[i]
+		} else {
+			res = os.Args[i]
+		}
+	}
+
+	fmt.Println(res)
+}
